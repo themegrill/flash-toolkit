ae20226075661845016a4f1d5b68f56e1a0852c1
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-12-16 14:49:21 +0545

    Fix - Conflict with flash theme dismiss notice

--- includes/admin/views/html-notice-custom.php
+++ includes/admin/views/html-notice-custom.php
@@ -9,6 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <div id="message" class="updated flash-toolkit-message">
-	<a class="flash-toolkit-message-close notice-dismiss" href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'flash-hide-notice', $notice ), 'flash_toolkit_hide_notices_nonce', '_flash_notice_nonce' ) ); ?>"><?php _e( 'Dismiss', 'flash-toolkit' ); ?></a>
+	<a class="flash-toolkit-message-close notice-dismiss" href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'flash-toolkit-hide-notice', $notice ), 'flash_toolkit_hide_notices_nonce', '_flash_toolkit_notice_nonce' ) ); ?>"><?php _e( 'Dismiss', 'flash-toolkit' ); ?></a>
 	<?php echo wp_kses_post( wpautop( $notice_html ) ); ?>
 </div>
