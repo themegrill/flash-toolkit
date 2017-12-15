cbb4a5cb1909b8bc9fb2f0a65f8f66d97d06012a
Bishal Napit <mebishalnapit@gmail.com>
2017-12-15 10:46:58 +0545

    Code reformat

--- includes/admin/meta-boxes/class-flash-meta-box-pageoptions-data.php
+++ includes/admin/meta-boxes/class-flash-meta-box-pageoptions-data.php
@@ -20,6 +20,7 @@ class FT_Meta_Box_Pageoptions_Data {
 
 	/**
 	 * Output the meta box.
+	 *
 	 * @param WP_Post $post
 	 */
 	public static function output( $post ) {
@@ -29,51 +30,52 @@ class FT_Meta_Box_Pageoptions_Data {
 		<div id="portfolio_options" class="panel-wrap portfolio_data">
 			<ul class="portfolio_data_tabs ft-tabs">
 				<?php
-					$portfolio_data_tabs = apply_filters( 'flash-toolkit_portfolio_data_tabs', array(
-						'general' => array(
-							'label'  => __( 'Page Header', 'flash-toolkit' ),
-							'target' => 'general_portfolio_data',
-							'class'  => array(),
-						)
-					) );
+				$portfolio_data_tabs = apply_filters( 'flash-toolkit_portfolio_data_tabs', array(
+					'general' => array(
+						'label'  => __( 'Page Header', 'flash-toolkit' ),
+						'target' => 'general_portfolio_data',
+						'class'  => array(),
+					),
+				) );
 
-					foreach ( $portfolio_data_tabs as $key => $tab ) {
-						?><li class="<?php echo $key; ?>_options <?php echo $key; ?>_tab <?php echo implode( ' ', (array) $tab['class'] ); ?>">
-							<a href="#<?php echo $tab['target']; ?>"><?php echo esc_html( $tab['label'] ); ?></a>
-						</li><?php
-					}
+				foreach ( $portfolio_data_tabs as $key => $tab ) {
+					?>
+				<li class="<?php echo $key; ?>_options <?php echo $key; ?>_tab <?php echo implode( ' ', (array) $tab[ 'class' ] ); ?>">
+					<a href="#<?php echo $tab[ 'target' ]; ?>"><?php echo esc_html( $tab[ 'label' ] ); ?></a>
+					</li><?php
+				}
 
-					do_action( 'flash_toolkit_pageoptions_write_panel_tabs' );
+				do_action( 'flash_toolkit_pageoptions_write_panel_tabs' );
 				?>
 			</ul>
 			<div id="general_portfolio_data" class="panel flash_toolkit_options_panel hidden"><?php
 
 				echo '<div class="options_group">';
 
-					// Page Header Size Type
-					flash_toolkit_wp_select( array(
-						'id'    => 'pageheader_size',
-						'label' => __( 'Page Header Size', 'flash-toolkit' ),
-						'options' => array(
-							'theme-options'  => __( 'From Theme Options', 'flash-toolkit' ),
-							'small'          => __( 'Small', 'flash-toolkit' ),
-							'medium'         => __( 'Medium', 'flash-toolkit' ),
-							'big'            => __( 'Big', 'flash-toolkit' ),
-						),
-					) );
+				// Page Header Size Type
+				flash_toolkit_wp_select( array(
+					'id'      => 'pageheader_size',
+					'label'   => __( 'Page Header Size', 'flash-toolkit' ),
+					'options' => array(
+						'theme-options' => __( 'From Theme Options', 'flash-toolkit' ),
+						'small'         => __( 'Small', 'flash-toolkit' ),
+						'medium'        => __( 'Medium', 'flash-toolkit' ),
+						'big'           => __( 'Big', 'flash-toolkit' ),
+					),
+				) );
 
 				echo '</div>';
 
 				echo '<div class="options_group">';
 
-					// Remove Breadcrumbs
-					flash_toolkit_wp_checkbox( array( 'id' => 'pageheader_disable', 'label' => __( 'Hide Pageheader', 'flash-toolkit' ), ) );
+				// Remove Breadcrumbs
+				flash_toolkit_wp_checkbox( array( 'id' => 'pageheader_disable', 'label' => __( 'Hide Pageheader', 'flash-toolkit' ), ) );
 
 				echo '</div>';
 
 				do_action( 'flash_toolkit_pageoptions_options_general' );
 
-			?></div>
+				?></div>
 			<?php do_action( 'flash_toolkit_pageoptions_data_panels' ); ?>
 			<div class="clear"></div>
 		</div>
@@ -82,12 +84,13 @@ class FT_Meta_Box_Pageoptions_Data {
 
 	/**
 	 * Save meta box data.
+	 *
 	 * @param int $post_id
 	 */
 	public static function save( $post_id ) {
 		// Add/replace data to array
-		$pageheader_size      = flash_clean( $_POST[ 'pageheader_size' ] );
-		$pageheader_disable = isset( $_POST['pageheader_disable'] ) ? 'yes' : 'no';
+		$pageheader_size    = flash_clean( $_POST[ 'pageheader_size' ] );
+		$pageheader_disable = isset( $_POST[ 'pageheader_disable' ] ) ? 'yes' : 'no';
 
 		// Save
 		update_post_meta( $post_id, 'pageheader_size', $pageheader_size );
