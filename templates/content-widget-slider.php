25c806962b909cc974f748670d49889bd430c07b
Ashish Shrestha <ashishsthanp@gmail.com>
2018-09-13 09:22:02 +0000

    Alt, title attributes in slider images

--- templates/content-widget-slider.php
+++ templates/content-widget-slider.php
@@ -19,41 +19,60 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-$screen            = isset( $instance[ 'full-screen'] ) ? $instance[ 'full-screen' ] : '' ;
+$screen            = isset( $instance['full-screen'] ) ? $instance['full-screen'] : '';
 $color             = isset( $instance['color'] ) ? $instance['color'] : 'slider-dark';
 $align             = isset( $instance['align'] ) ? $instance['align'] : 'slider-content-center';
 $controls          = isset( $instance['controls'] ) ? $instance['controls'] : 'slider-control-center';
 $repeatable_slider = isset( $instance['repeatable_slider'] ) ? $instance['repeatable_slider'] : array();
 
-if( $screen ) {
+if ( $screen ) {
 	$slide_status = 'full-screen';
 } else {
 	$slide_status = 'full-width';
 }
 ?>
- <div class="tg-slider-widget <?php echo esc_attr( $color ); ?> <?php echo esc_attr( $align ); ?> <?php echo esc_attr( $controls ); ?> <?php echo esc_attr( $slide_status) ; ?>">
+<div class="tg-slider-widget <?php echo esc_attr( $color ); ?> <?php echo esc_attr( $align ); ?> <?php echo esc_attr( $controls ); ?> <?php echo esc_attr( $slide_status ); ?>">
 	<div class="swiper-container">
 		<div class="swiper-wrapper">
-			<?php foreach ($repeatable_slider as $slider) {
-			if( $slider['image'] != '' ) { ?>
-			<div class="swiper-slide">
-				<figure class="slider-image" <?php if ( 'full-screen' == $slide_status ) { echo 'style="background-image: url(' . esc_url( $slider['image'] ) .')"'; } ?>>
-					<img src="<?php echo esc_html( $slider[ 'image' ] ); ?>"/>
-					<div class="overlay"></div>
-				</figure>
-				<div class="slider-content">
-					<div class="tg-container">
-						<div class="caption-title"><?php echo esc_html( $slider[ 'title' ] ); ?></div>
-						<div class="caption-desc"><?php echo wp_kses_post( $slider[ 'description' ] ); ?></div>
-						<?php if($slider['button_text']) : ?>
-						<div class="btn-wrapper">
-							<a href="<?php echo esc_url( $slider[ 'button_link' ] ); ?>"><?php echo esc_html( $slider[ 'button_text' ] ); ?></a>
+			<?php foreach ( $repeatable_slider as $slider ) {
+				if ( $slider['image'] != '' ) {
+					$image_url  = $slider['image'];
+					$image_id   = attachment_url_to_postid( $image_url );
+					$image_data = wp_get_attachment_image_src( $image_id, 'full' );
+
+					// Dimensions
+					$width  = ( $image_id ) ? 'width="' . $image_data[1] . '"' : '';
+					$height = ( $image_id ) ? 'height="' . $image_data[2] . '"' : '';
+
+					// Attributes.
+					$alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+					$alt = ! empty( $alt ) ? $alt : $slider['title'];
+					?>
+
+					<div class="swiper-slide">
+						<figure class="slider-image" <?php if ( 'full-screen' == $slide_status ) {
+							echo 'style="background-image: url(' . esc_url( $slider['image'] ) . ')"';
+						} ?>>
+							<img <?php echo $width . ' ' . $height; ?>
+									src="<?php echo esc_html( $slider['image'] ); ?>"
+									alt="<?php echo esc_attr( $alt ); ?>"
+									title="<?php echo esc_attr( $slider['title'] ); ?>"/>
+							<div class="overlay"></div>
+						</figure>
+						<div class="slider-content">
+							<div class="tg-container">
+								<div class="caption-title"><?php echo esc_html( $slider['title'] ); ?></div>
+								<div class="caption-desc"><?php echo wp_kses_post( $slider['description'] ); ?></div>
+								<?php if ( $slider['button_text'] ) : ?>
+									<div class="btn-wrapper">
+										<a href="<?php echo esc_url( $slider['button_link'] ); ?>"><?php echo esc_html( $slider['button_text'] ); ?></a>
+									</div>
+								<?php endif; ?>
+							</div>
 						</div>
-						<?php endif; ?>
 					</div>
-				</div>
-			</div>
-			<?php }
+
+				<?php }
 			} ?>
 		</div>
 		<div class="swiper-pagination"></div>
